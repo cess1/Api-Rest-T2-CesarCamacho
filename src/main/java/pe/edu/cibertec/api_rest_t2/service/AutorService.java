e970e584a8faa189149234a83903595163370251
cess1 <cescamacho94@gmail.com>
2024-06-23 12:49:13 -0500

    avance2

--- src/main/java/pe/edu/cibertec/api_rest_t2/service/AutorService.java
+++ src/main/java/pe/edu/cibertec/api_rest_t2/service/AutorService.java
@@ -1,4 +1,35 @@
 package pe.edu.cibertec.api_rest_t2.service;
 
-public class AutorService {
+import org.springframework.stereotype.Service;
+import pe.edu.cibertec.api_rest_t2.model.bd.Autor;
+import pe.edu.cibertec.api_rest_t2.repository.AutorRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+public class AutorService implements IAutorService{
+    private AutorRepository autorRepository;
+
+    @Override
+    public List<Autor> findAll() {
+        return autorRepository.findAll();
+    }
+
+    @Override
+    public Optional<Autor> findById(int id) {
+        return autorRepository.findById(id);
+    }
+
+    @Override
+    public Autor save(Autor autor) {
+        return autorRepository.save(autor);
+    }
+
+    @Override
+    public void deleteById(int id) {
+        autorRepository.deleteById(id);
+    }
 }
+
